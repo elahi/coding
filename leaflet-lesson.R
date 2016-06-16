f9e8cb0a21e6198f8622b92e2fcc6f75b36c6da7
Robin Elahi <elahi.robin@gmail.com>
2016-06-15 17:04:30 -0700

    learned some leaflet

---
+++ leaflet-lesson.R
@@ -0,0 +1,61 @@
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+
+m  # Print the map
+
+
+# add some circles to a map
+df = data.frame(Lat = 1:10, Long = rnorm(10))
+leaflet(df) %>% addCircles()
+
+leaflet(df) %>% addCircles(lng = ~Long, lat = ~Lat)
+
+leaflet() %>% addCircles(data = df)
+# or use df in addCircles() only
+leaflet() %>% addCircles(data = df, lat = ~ Lat, lng = ~ Long)
+
+library(sp)
+Sr1 = Polygon(cbind(c(2, 4, 4, 1, 2), c(2, 3, 5, 4, 2)))
+Sr2 = Polygon(cbind(c(5, 4, 2, 5), c(2, 3, 2, 2)))
+Sr3 = Polygon(cbind(c(4, 4, 5, 10, 4), c(5, 3, 2, 5, 5)))
+Sr4 = Polygon(cbind(c(5, 6, 6, 5, 5), c(4, 4, 3, 3, 4)), hole = TRUE)
+Srs1 = Polygons(list(Sr1), "s1")
+Srs2 = Polygons(list(Sr2), "s2")
+Srs3 = Polygons(list(Sr4, Sr3), "s3/4")
+SpP = SpatialPolygons(list(Srs1, Srs2, Srs3), 1:3)
+leaflet(height = "300px") %>% addPolygons(data = SpP)
+
+library(maps)
+mapStates = map("state", fill = TRUE, plot = FALSE)
+leaflet(data = mapStates) %>% addTiles() %>%
+  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+
+m = leaflet() %>% addTiles()
+df = data.frame(
+  lat = rnorm(100),
+  lng = rnorm(100),
+  size = runif(100, 5, 20),
+  color = sample(colors(), 100)
+)
+m = leaflet(df) %>% addTiles()
+m %>% addCircleMarkers(radius = ~size, color = ~color, fill = FALSE)
+m %>% addCircleMarkers(radius = runif(100, 4, 10), color = c('red'))
+
+#### QUAKES
+data(quakes)
+
+# Show first 20 rows from the `quakes` dataset
+leaflet(data = quakes[1:20,]) %>% addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(mag))
+
+
+##### ADRIATIC EXAMPLE ####
+library(ggplot2)
+
+m <- leaflet() %>% setView(lng = 15.5, lat = 43.3, zoom = 6)
+m %>% addTiles() 
+ggsave("leaflet1.jpg", height = 3.5, width = 3.5)
+m %>% addProviderTiles("CartoDB.Positron")
