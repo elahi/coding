6c517d60a53c2d1174e8f39e328c8ace4456172c
Robin Elahi <elahi.robin@gmail.com>
2018-05-14 20:12:17 -0700

    edited ggplot2 tutorial

--- carpentry_training/04-visualization-ggplot2.R
+++ carpentry_training/04-visualization-ggplot2.R
@@ -19,32 +19,42 @@
 ##' (column for every dimension, a row for every observation)
 ##' well-structured data will save you lots of time
 
-##### GEOM_POINT #####
-
-library(tidyverse)
-library(hexbin)
-
-surveys_complete <- read_csv("carpentry_training/data_output/surveys_complete.csv")
-
-##' basic template:
+##' basic grammar template:
 ##' ggplot(data = , mapping = aes()) + GEOM_FUNCTION
 ##' ggplot(data = surveys_complete)
-##' ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length)) 
+##' ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length))
+
+##' Emphasis on visualizing the data
+##' Scatterplot
+##' Histogram
+##' Boxplot
+##' Means + standard deviations
+
+library(tidyverse)
+library(viridis) # for color-blind friendly plots
+
+## Load surveys_complete data that we prepared last week
+surveys_complete <- read_csv("carpentry_training/data_output/surveys_complete.csv")
+
+##### GEOM_POINT #####
+
 ggplot(data = surveys_complete, 
        mapping = aes(x = weight, y = hindfoot_length)) + geom_point()
 
+## Save the mapping
 surveys_plot <- ggplot(data = surveys_complete, aes(weight, hindfoot_length))
 
 surveys_plot + geom_point()
 
-## Add color to all the points
+## Add color to all the points, alpha
 ggplot(data = surveys_complete, aes(weight, hindfoot_length)) + 
   geom_point(alpha = 0.1, color = "blue")
 
 ## Color by species_id
 ggplot(data = surveys_complete, 
        aes(weight, hindfoot_length, color = species_id)) + 
-  geom_point(alpha = 0.1)
+  geom_point(alpha = 0.3) + 
+  scale_color_viridis_d()
 
 ### Challenge 1 ###
 # Use what you just learned to create a scatter plot of weight over species_id with the plot types showing in different colors. 
@@ -52,8 +62,6 @@ ggplot(data = surveys_complete,
 
 ggplot(data = surveys_complete, aes(x = species_id, y = weight)) +
   geom_point(aes(color = plot_type))
-
-# No - transition to boxplots
 
 ##### GEOM_BOXPLOT #####
 
@@ -100,20 +108,21 @@ ggplot(data = surveys_complete, aes(species_id, weight)) +
 
 # 3. 
 ggplot(data = surveys_complete, 
+       aes(species_id, hindfoot_length)) + 
+  geom_jitter(alpha = 0.3, color = "red") + 
+  geom_boxplot(alpha = 0.7)
+
+# 4. 
+ggplot(data = surveys_complete, 
        aes(species_id, hindfoot_length, color = plot_id)) + 
   geom_jitter(alpha = 0.3) + 
-  geom_boxplot(alpha = 0) 
+  geom_boxplot(alpha = 0.7) 
 
 ggplot(data = surveys_complete, 
        aes(species_id, hindfoot_length, 
            color = as.character(plot_id))) + 
   geom_jitter(alpha = 0.3) + 
-  geom_boxplot(alpha = 0) 
-
-ggplot(data = surveys_complete, 
-       aes(species_id, hindfoot_length)) + 
-  geom_jitter(alpha = 0.3, aes(color = as.character(plot_id))) + 
-  geom_boxplot(alpha = 0) 
+  geom_boxplot(alpha = 0.7) 
 
 ## Reorder by weight
 str(surveys_complete)
@@ -124,6 +133,26 @@ str(surveys_complete)
 ggplot(data = surveys_complete, 
        aes(species_id, weight)) + 
   geom_boxplot(alpha = 0) 
+
+##### GEOM_HIST #####
+
+surveys_complete %>% 
+  ggplot(aes(weight)) + 
+  geom_histogram() + 
+  facet_wrap(~ species_id)
+
+## Focus on one species
+surveys_complete %>% 
+  filter(species_id == "DM") %>%
+  ggplot(aes(weight)) + 
+  geom_histogram() + 
+  facet_wrap(~ species_id)
+
+## Focus on one species, by sex; adjust bin width
+surveys_complete %>% 
+  filter(species_id == "DM") %>%
+  ggplot(aes(weight, fill = sex)) + 
+  geom_histogram(alpha = 0.5, binwidth = 1) 
 
 ##### GEOM_LINE #####
 
@@ -195,10 +224,12 @@ ggplot(data = surveys_complete, aes(species_id, weight)) +
 #' hint 1: use the size argument in the aes() mapping)
 #' hint 2: http://ggplot2.tidyverse.org/reference/geom_point.html
 
-#' 4. Recreate 'surveys_summary', but now add the standard error
-#' (google the formula for standard error if you have not learned this or forgotten)
+#' 4. On the previous plot, add error bars for the standard deviation
+#' hint(use a new layer, called geom_errorbar())
 
-#' 5. Now on the plot, add error bars for the standard deviation
+#' 5. Recreate 'surveys_summary', but now add the standard error
+#' (google the formula for standard error if you have not learned this or forgotten)
+#' Now on the plot, add error bars for the standard deviation
 #' hint(use a new layer, called geom_errorbar())
 
 # 1. 
@@ -227,7 +258,7 @@ surveys_summary %>%
   geom_errorbar(aes(ymin = wt_mean - wt_sd, 
                 ymax = wt_mean + wt_sd))
 
-# 5. Get standard errors and plot those
+# 5. Get standard errors 
 surveys_summary <- surveys_complete %>% 
   filter(!is.na(weight)) %>% 
   group_by(species_id) %>% 
@@ -241,6 +272,7 @@ surveys_summary %>%
   geom_point(aes(size = n)) + 
   geom_errorbar(aes(ymin = wt_mean - wt_se, 
                     ymax = wt_mean + wt_se))
+
 
 ## Introduce 95% CIs and plot those
 surveys_summary <- surveys_complete %>% 
@@ -271,21 +303,58 @@ surveys_summary %>%
 
 #### EXPORTING ####
 
-## Tweaking the plot
 surveys_summary %>% 
   ggplot(aes(species_id, wt_mean)) + 
-  geom_point(aes()) + 
+  geom_point(aes(size = n)) + 
+  geom_errorbar(aes(ymin = wt_mean - wt_sd, 
+                    ymax = wt_mean + wt_sd)) + 
+  theme_bw(base_size = 14)
+
+ggsave("carpentry_training/fig_output/fig1.pdf", 
+       height = 3.5, width = 5)
+
+### Challenge 4 ###
+## Tweaking the plot
+## Try to change axis labels; add figure title (hint: labs())
+## Try to remove the gray panel gridlines (hint: theme())
+## Try to change the error bar widths (hint: geom_errorbar())
+## Try to place the legend on the bottom of the figure
+# https://www.rstudio.com/wp-content/uploads/2015/08/ggplot2-cheatsheet.pdf
+
+surveys_summary %>% 
+  ggplot(aes(species_id, wt_mean)) + 
+  geom_point(aes(size = n)) + 
   geom_errorbar(aes(ymin = wt_mean - wt_sd, 
                     ymax = wt_mean + wt_sd), 
                 width = 0, size = 0.5) + 
-  geom_errorbar(aes(ymin = wt_mean - wt_ci, 
-                    ymax = wt_mean + wt_ci), 
-                width = 0, size = 1, color = "red") + 
   labs(title = "Portal LTER mammal species", 
        x = "Species code", 
        y = "Weight (g)") + 
   theme_bw(base_size = 14) + 
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank()) + 
+  theme(legend.position = "bottom")
+
+ggsave("carpentry_training/fig_output/fig1.pdf", 
+       height = 3.5, width = 5)
+
+### Challenge 5 ###
+## Try to add the original data points to the plot
+## Hint: use geom_jitter as before
+
+surveys_summary %>% 
+  ggplot(aes(species_id, wt_mean)) + 
+  geom_jitter(data = surveys_complete, aes(species_id, weight), 
+              alpha = 0.25, size = 0.75, width = 0.1, color = "lightblue") + 
+  geom_point(aes()) + 
+  geom_errorbar(aes(ymin = wt_mean - wt_sd, 
+                    ymax = wt_mean + wt_sd), 
+                width = 0, size = 0.5) + 
+  labs(title = "Portal LTER mammal species", 
+       x = "Species code", 
+       y = "Weight (g)") + 
+  theme_bw(base_size = 14) + 
+  theme(panel.grid = element_blank()) + 
+  theme(legend.position = "bottom")
 
 ggsave("carpentry_training/fig_output/fig1.pdf", 
        height = 3.5, width = 5)
@@ -293,7 +362,7 @@ ggsave("carpentry_training/fig_output/fig1.pdf",
 #### EXTRAS...#####
 
 ## Themes
-install.packages("ggthemes")
+#install.packages("ggthemes")
 library(ggthemes)
 
 my_plot <- yearly_sex_counts %>% 
@@ -301,19 +370,15 @@ my_plot <- yearly_sex_counts %>%
   geom_line() + 
   facet_wrap(~ species_id)
 
-my_plot + theme_few() 
-my_plot + theme_par() 
+# my_plot + theme_few() 
+# my_plot + theme_par() 
 my_plot + theme_tufte() 
 my_plot + theme_wsj() 
 my_plot + theme_fivethirtyeight() 
-my_plot + theme_base() 
+my_plot + theme_excel()
 
-##
-yearly_sex_weight <- surveys_complete %>%
-  group_by(year, sex, species_id) %>%
-  summarize(avg_weight = mean(weight))
 
-# Challenge 4
+# Challenge 6
 # 
 # With all of this information in hand, please take another five minutes to either improve one of the plots generated in this exercise or create a beautiful graph of your own. Use the RStudio ggplot2 cheat sheet for inspiration. 
 
@@ -324,5 +389,3 @@ yearly_sex_weight <- surveys_complete %>%
 #   See if you can change the thickness of the lines.
 # Can you find a way to change the name of the legend? What about its labels?
 #   Try using a different color palette (see http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/).
-
-
