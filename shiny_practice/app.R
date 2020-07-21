3b04ad16c28b583ae1e94adcff3d8bc9c4432fbe
Robin Elahi <elahi@Robins-MacBook-Pro-2.local>
2020-07-21 12:26:45 -0700

    update shiny app

--- shiny_practice/app.R
+++ shiny_practice/app.R
@@ -1,16 +1,43 @@
 library(shiny)
+library(tidyverse)
+
+###### FUNCTIONS ######
+
+disc.logistic <- function(n, p) { 
+  r <- unname(p['r']) ## set the growth rate (dropping the parameter name with 'unname')
+  K <-  unname(p['K']) ## set the carrying capacity
+  n1 <- n + r * n * (1 - n / K) ## calculate the new population size
+  return(n1) ## and return it 
+}
+
+disc.logistic.2 <- function(n, p, T) { ## T is the total number of time steps
+  N <- vector(length=T+1)
+  N[1] <- n ## set the initial value
+  for (t in 1:T) { 
+    N[t+1] <- disc.logistic(N[t], p) ## we can call the function we wrote above to calculate the new population size
+  }
+  return(N) ## return the vector of population sizes
+}
+
+##### SHINY ######
 
 ui <- fluidPage(
-  sliderInput(inputId = "num", 
-              label = "Choose a number", 
-              value = 25, min = 1, max = 100), 
-  plotOutput("hist")
+  sliderInput(inputId = "K", 
+              label = "Choose carrying capacity", 
+              value = 100, min = 10, max = 500), 
+  plotOutput("timeseries")
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot({
-    title <- "n random normal values"
-    hist(rnorm(input$num), main = title)
+  output$timeseries <- renderPlot({
+    n0 <- 1
+    p <- c(r = 0.5, K = input$K) 
+    n_years <- 50
+    n <- disc.logistic.2(n = n0, p = p, T = n_years)
+    plot(x = 0:n_years, y = n, type = 'l', 
+         xlab = 'Time', ylab = 'Population size', 
+         main = 'Discrete time logistic model')
+    
   })
 }
 
