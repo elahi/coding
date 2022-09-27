ff499f357551b837a20f403f4bf1fbc9e5e102ff
Robin Elahi <elahi@DN5272g4.SUNet>
2022-09-27 12:29:11 -0700

    updated r etc

--- ipak.R
+++ ipak.R
@@ -1,12 +1,8 @@
 ################################################################################
 ##' @title Package installation
-##'
 ##' @author Robin Elahi
-##' @contact elahi.robin@gmail.com
-##' 
 ##' @date 2017-05-01
-##' 
-##' @log 
+##' @log 2022-09-27 updated packages
 ################################################################################
 
 # https://gist.github.com/stevenworthington/3178163
@@ -22,25 +18,55 @@ ipak <- function(pkg){
 }
 
 # usage
-packages <- c("ggplot2", "dplyr", "reshape2", "RColorBrewer", "scales", "grid",
-              "tidyr", "lme4", "nlme", "lubridate", "readr",
-              "sp", "spatstat", "leaflet", "broom",
-              "rgdal", "raster", "marmap", "mapdata", "rtide", "ggmap",
-              "AICcmodavg", "tibble")
-
-## without tidyverse
-packages <- c("reshape2", "RColorBrewer", "scales", "grid", 
-              "lme4", "nlme", 
-              "sp", "spatstat", "leaflet", "broom", 
-              "rgdal", "raster", "marmap", "mapdata", "rtide", "ggmap", 
-              "AICcmodavg")
-
+packages <- c("tidyverse", "lme4", "nlme")
 ipak(packages)
 
-## rethinking
-packages <- (c("mvtnorm", "loo", "coda"))
-ipak(packages)
+# rstan
+# first install C++ toolchain
+# https://github.com/rmacoslib/r-macos-rtools#how-do-i-use-the-installer 
+# note that the above probably works even though there is a failure message at the end; see
+# https://discourse.mc-stan.org/t/friendly-mac-c-installer-does-not-work-for-me-whats-another-option/27272/4
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# verify installation
+example(stan_model, package = "rstan", run.dontrun = TRUE)
+summary(fit)
 
-options(repos=c(getOption('repos'), rethinking='http://xcelab.net/R'))
-install.packages('rethinking',type='source')
+# cmdstan
+# https://mc-stan.org/cmdstanr/articles/cmdstanr.html
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+library(cmdstanr)
+install_cmdstan()
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+library(posterior)
+library(bayesplot)
+color_scheme_set("brightblue")
+check_cmdstan_toolchain()
+cmdstan_path()
+cmdstan_version()
+file <- file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.stan")
+file
+mod <- cmdstan_model(file)
+mod$print
+mod$exe_file()
+# names correspond to the data block in the Stan program
+data_list <- list(N = 10, y = c(0,1,0,0,0,0,0,0,0,1))
 
+fit <- mod$sample(
+  data = data_list, 
+  seed = 123, 
+  chains = 4, 
+  parallel_chains = 4,
+  refresh = 500 # print update every 500 iters
+)
+
+fit$summary()
+
+# rethinking
+install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
+
+# bayes rules
+install.packages(c("bayesrules", "janitor", "rstanarm",
+                   "bayesplot", "tidybayes", "broom.mixed", "modelr",
+                   "e1071", "forcats"), 
+                 dependencies = TRUE)
